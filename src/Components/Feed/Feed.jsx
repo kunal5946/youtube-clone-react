1ef0040013274c4ba81eedc917354d43c1eb033b
kunal5946 <kunalsripalanchu@gmail.com>
2026-02-19 18:59:52 +0530

    changed code to not fail when api fetching fails

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -8,47 +8,53 @@ import thumbnail5 from '../../assets/thumbnail5.png'
 import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
-import {Link} from 'react-router-dom'
-import {API_KEY} from '../../data.js'
-import {useState,useEffect} from 'react'
-import {value_converter} from '../../data.js'
+import { Link } from 'react-router-dom'
+import { API_KEY } from '../../data.js'
+import { useState, useEffect } from 'react'
+import { value_converter } from '../../data.js'
 import moment from 'moment'
 
 
-const Feed = ({category}) => {
-    const [data_arr,setData]=useState([]);
+const Feed = ({ category }) => {
+    const [data_arr, setData] = useState([]);
 
-    const fetchData= async()=>{
+    const fetchData = async () => {
 
-        const videolist_url=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
-        await fetch(videolist_url).then(response=>response.json()).then(data=>setData(data.items))
+        const videolist_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+        await fetch(videolist_url).then(response => response.json()).then(data => {
+            if (data.items) {
+                setData(data.items);
+            } else {
+                console.error("API Error or No Items:", data);
+            }
+        })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
-        },[category]
+    }, [category]
     )
 
-  return (
-    <div className="feed">
-        {   data_arr.map((current_item,index)=>{
-                return(
-                        //the item.snippet.categoryId and item.id are category id and video id and on click and when change in url we use useparam to extract videoid and categoryid and pass them to playvideo component
+    return (
+        <div className="feed">
+            {data_arr?.map((current_item, index) => {
+                return (
+                    //the item.snippet.categoryId and item.id are category id and video id and on click and when change in url we use useparam to extract videoid and categoryid and pass them to playvideo component
                     <Link to={`video/${current_item.snippet.categoryId}/${current_item.id}`} className="card">
-                        <img  src={current_item.snippet.thumbnails.medium.url} alt= ""/>
+                        <img src={current_item.snippet.thumbnails.medium.url} alt="" />
                         <h2>{current_item.snippet.title}</h2>
                         <h3>{current_item.snippet.channelTitle}</h3>
-                        <p>{value_converter(current_item.statistics.viewCount)} views &bull; {moment(current_item.snippet.publishedAt).fromNow() }</p>
+                        <p>{value_converter(current_item.statistics.viewCount)} views &bull; {moment(current_item.snippet.publishedAt).fromNow()}</p>
                     </Link>
 
-                     )
-                }
+                )
+            }
             )
-        }
-        
-       
-    </div>
-  )
+            }
+
+
+        </div>
+    )
 }
 
 export default Feed
